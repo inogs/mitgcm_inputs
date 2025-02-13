b37e316791f669ae77dfdf21d0c486befee40a96
apetronio-OGS <apetronio@ogs.it>
2025-02-13 10:40:11 +0100

    python input file with paths to input data and output folders v0

---
+++ src/mitgcm_inputs/pathes_to_data.py
@@ -0,0 +1,20 @@
+# pathes_to_data
+# project path
+prj_path = "/home/andrea/Codes/pyScript/"
+out_path = "output_data/"
+in_path = "input_data/"
+# input_data
+# MED results
+med_data_path = (
+    prj_path + in_path + "MITgcm_BFM/lightExt/xESP_forc_days1to1920.bin"
+)
+med_grid_path = prj_path + in_path + "MITgcm_BFM/CLR_365days/results_merged/"
+bathymetry_path = prj_path + in_path + "bathymetry/bathydata_GoT_iNEST_V2/"
+# mask
+mask_path = prj_path + out_path + "mask/"
+# bottom fluxes + flat GoT
+bottom_fluxes_path = prj_path + out_path + "bottom_fluxes/"
+# surface deposition
+fluxes_path = prj_path + out_path + "fluxes/"
+# K extintion
+kext_path = prj_path + out_path + "Kext/"
