147b992c3b1f8176579aeaab316ceb7bfbfde2ac
Giorgio Bolzon <gbolzon@ogs.it>
2025-05-20 10:00:34 +0200

    Importing code and data from https://github.com/inogs/MITgcm_BC_IC/tree/main/mitgcm/bc_ic/static-data/masks/MER

---
+++ src/mitgcm_inputs/rbcs/point_sources_n_rivers.py
@@ -0,0 +1,220 @@
+import numpy as np
+import xarray as xr
+import pandas as pd
+import sys
+import json
+
+#
+
+def initialise_sal(
+		hFac,
+):
+	"""
+	Given the hFac mask from the static files generation,
+	returns a zero-fill 3D array to be filled with
+	salinity relaxation values for the bottom concentration
+	point sources.
+	"""
+
+	return xr.zeros_like(hFac)
+
+def initialise_conc_fluxes(
+		hFac,
+):
+	"""
+	Given the hFac mask from the static files generation,
+	returns a zero-fill 2D array to be filled with
+	concentration values for the bottom concentration
+	point sources or surface river sources.
+	"""
+
+	return xr.zeros_like(hFac[0,:,:])
+
+#
+
+def get_spatial_masks(
+		ds,
+):
+	"""
+	Given the dataset from the static files generation,
+	returns a zero-fill 2D array to be filled with
+	concentration values for the bottom concentration
+	point sources or surface river sources.
+	"""
+
+	depth = ds.Depth
+	hFac = ds.hFacC
+	xc = ds.XC
+	yc = ds.YC
+	zc = ds.Z
+
+	return {'xc': xc, 'yc': yc, 'zc': zc, 'depth': depth, 'hFac': hFac}
+
+def open_point_sources(
+		json_file,
+):
+	"""
+	Given the json of a specific domain's point sources,
+	reads the data within.
+	"""
+
+	with open(json_file, 'r') as jfile:
+		jdata = json.load(jfile)
+	return jdata['discharge_points']
+
+def open_river_sources(
+		json_file,
+):
+	"""
+	Given the json of a specific domain's river sources,
+	reads the data within.
+	"""
+
+	with open(json_file, 'r') as jfile:
+		jdata = json.load(jfile)
+	return jdata['river_points']
+
+#
+
+def fill_sal_conc(
+		relax_sal,
+		conc,
+		x,
+		y,
+		z,
+		depth,
+		json_data,
+		water_freshener = 0.5,
+		only_one = False
+):
+	"""
+	Given the info for the point sources and the spatial static data,
+	fills the relaxation salinity array, creates the salinity mask and
+	fills the concentration array.
+	"""
+	if only_one:
+		jd = json_data[0]
+		lon = jd['Long']
+		lat = jd['Lat']
+		i = np.argmin(np.abs(x.values - lon))
+		j = np.argmin(np.abs(y.values - lat))
+		k = np.argmin(np.abs(z.values + depth[j, i].values))
+		rel_S = jd['CMS_avgS'] - water_freshener
+		c = jd['Carico_Ingresso_AE'] * jd['Dilution_factor']
+		relax_sal[k, j, i] = rel_S
+		conc[j, i] = c
+	else:
+		for jd in json_data:
+			lon = jd['Long']
+			lat = jd['Lat']
+			i = np.argmin(np.abs(x.values - lon))
+			j = np.argmin(np.abs(y.values - lat))
+			k = np.argmin(np.abs(z.values + depth[j,i].values))
+			rel_S = jd['CMS_avgS'] - water_freshener
+			c = jd['Carico_Ingresso_AE'] * jd['Dilution_factor']
+			relax_sal[k,j,i] = rel_S
+			conc[j,i] = c
+	return relax_sal, xr.where(relax_sal == 0., 0., 1.), conc
+
+#
+
+def write_binary_files(
+		relax_sal,
+		S_mask,
+		conc,
+		aggregated = True,
+		out_dir = 'data/',
+		relax_path = 'relax_salinity.bin',
+		conc_path = 'tracer_concetrations.bin',
+		mask_path = 'S_source_mask_MER_V3.dat',
+		aggreg_path = 'sewage_discharges.nc',
+		conc_in_time = 365,
+):
+	"""
+	Given the the relaxation salinity, salinity mask and concentration
+	arrays, writes them to MITgcm-appropriate binary files.
+	"""
+	
+	relax_sal.values.astype('f4').tofile(out_dir+relax_path)
+	if conc_in_time == 1:
+		conc.values.astype('f4').tofile(out_dir+conc_path)
+	else:
+		np.stack([conc.values]*conc_in_time).astype('f4').tofile(out_dir+conc_path)
+	S_mask.values.astype('f4').tofile(out_dir+mask_path)
+	if aggregated:
+		xr.merge([relax_sal.rename('relax_salinity'), conc.rename('tracer_conc')]).to_netcdf(out_dir+aggreg_path)
+
+#
+
+def fill_river_conc(
+		conc,
+		x,
+		y,
+		z,
+		depth,
+		json_data,
+		water_freshener = 0.5,
+		only_one = False,
+):
+	"""
+	Given the info for the point sources and the spatial static data,
+	fills the relaxation salinity array, creates the salinity mask and
+	fills the concentration array.
+	"""
+	#TODO...
+	if only_one:
+		jd = json_data[0]
+		lon = jd['Long']
+		lat = jd['Lat']
+		i = np.argmin(np.abs(x.values - lon))
+		j = np.argmin(np.abs(y.values - lat))
+		k = np.argmin(np.abs(z.values + depth[j, i].values))
+		c = jd['Carico_Ingresso_AE'] * jd['Dilution_factor']
+		relax_sal[k, j, i] = rel_S
+		conc[j, i] = c
+	else:
+		for jd in json_data:
+			lon = jd['Long']
+			lat = jd['Lat']
+			i = np.argmin(np.abs(x.values - lon))
+			j = np.argmin(np.abs(y.values - lat))
+			k = np.argmin(np.abs(z.values + depth[j,i].values))
+			c = jd['Carico_Ingresso_AE'] * jd['Dilution_factor']
+			relax_sal[k,j,i] = rel_S
+			conc[j,i] = c
+	return relax_sal, xr.where(relax_sal == 0., 0., 1.), conc
+
+#
+
+
+###
+
+def main():
+	base_path = sys.argv[1]
+	domain = sys.argv[2]
+	static_path = base_path + domain + '/'
+	print('\n', static_path, '\n')
+	relax_salt = initialise_sal(xr.open_dataset(static_path + 'MIT_static_' + domain + '.nc').hFacC)
+	tracer_conc = initialise_conc_fluxes(xr.open_dataset(static_path + 'MIT_static_' + domain + '.nc').hFacC)
+	coords = get_spatial_masks(xr.open_dataset(static_path + 'MIT_static_' + domain + '.nc'))
+	
+	sewage_path = base_path + domain + f'/PointSource_wSalt_{domain}.json'
+	sewers = open_point_sources(sewage_path)
+	
+	relax_salt, mask_salt, tracer_conc = fill_sal_conc(relax_salt, tracer_conc, coords['xc'], coords['yc'], coords['zc'], coords['depth'], sewers, only_one=True)
+	write_binary_files(relax_salt, mask_salt, tracer_conc, out_dir=static_path,
+					   relax_path='bottom_sources_S_relaxation_'+domain+'.bin', conc_path='conc01_bottom_fluxes_'+domain+'.bin',
+					   mask_path='bottom_sources_S_mask_'+domain+'.bin', aggreg_path='sewage_discharges_'+domain+'.bin')
+	#
+	rivers_path = base_path + domain + f'/RiverSource_{domain}.json'
+	rivers = open_river_sources(rivers_path)
+
+
+
+#
+
+if __name__ == '__main__':
+    main()
+    print('DONE!')
+
+###
---
+++ src/mitgcm_inputs/rbcs/read_excelAllegato_1_do_json_for_7domains_with_salinity.py
@@ -0,0 +1,136 @@
+# read the Allegato1 excel file and select the domain from 1 to 7 and 
+# build the json files with the coordinate of the pointsource for E.Coli tracers
+import numpy as np
+import pandas as pd
+import json
+import os
+import copernicusmarine as cm
+import sys
+
+# Allegato 1 of sewage discharge locations 
+excel_file = 'Allegato_1_Capitolato_Tecnico_B32_B35_modificato.xlsx'
+base_path = sys.argv[1]
+data_path = sys.argv[2]
+# select the n. of domain from 1 to 7 and name the json file
+WHICH_DOMAIN=1
+#output_file = 'PointSource_nordadri.json'
+#domains=['NAD','SAD','ION','SIC','TYR','LIG','SAR']
+domains=['NAD','SAD','ION','SIC','TYR','LIG','SAR', 'GoT', 'GSN']
+
+
+# subset and read CMS reanalysis for salinity
+x0, x1 = 6.5000, 21.0000 #12.22265625, 16.07421875
+y0, y1 = 35.0000, 45.80859375 #43.47265625, 45.80859375
+z0, z1 = 1.0182366371154785, 5754.
+
+t0 = '2012-01-01T00:00:00'
+t1 = '2021-12-31T00:00:00'
+
+dataset = 'med-cmcc-sal-rean-d'
+var = 'so'
+
+sal = cm.open_dataset(
+    username = '...',
+    password = '...',
+    dataset_id = dataset,
+    #dataset_version = version,
+    variables = [var],
+    minimum_longitude = x0,
+    maximum_longitude = x1,
+    minimum_latitude = y0,
+    maximum_latitude = y1,
+    start_datetime = t0,
+    end_datetime = t1,
+    minimum_depth = z0,
+    maximum_depth = z1,
+)
+
+Lon, Lat = np.meshgrid(sal.longitude.values, sal.latitude.values)
+Lon = Lon * np.where(sal.so[0,0,:,:] == sal.so[0,0,:,:], 1, np.nan)
+Lat = Lat * np.where(sal.so[0,0,:,:] == sal.so[0,0,:,:], 1, np.nan)
+
+#import matplotlib.pyplot as plt
+#import cmocean as cmo
+#sal.so[0,0,:,:].plot.imshow(); plt.show()
+
+dataset = 'cmems_mod_med_phy_my_4.2km_static'
+var = 'deptho_lev'
+
+bathyCMS = cm.open_dataset(
+    username = '...',
+    password = '...',
+    dataset_id = dataset,
+    #dataset_version = version,
+    variables = [var],
+    minimum_longitude = x0,
+    maximum_longitude = x1,
+    minimum_latitude = y0,
+    maximum_latitude = y1,
+    minimum_depth = z0,
+    maximum_depth = z1,
+)['deptho_lev']
+
+print('Loaded datasets!')
+
+# Extract the Excel file name without extension
+file_name = os.path.splitext(os.path.basename(excel_file))[0]
+
+# Read the Excel file into a DataFrame
+df = pd.read_excel(data_path+excel_file)
+
+for id, namedomain in enumerate(domains):
+
+  # Filter rows where the value in column 'Dominio' is id+1 (valid number from 1 to 7)
+  if id < 7:
+      filtered_df = df[df['Dominio'] == id + 1]
+  else:
+      filtered_df = df[df['Sotto-Dominio'] == 8]  ### hardcoded horribly !!!
+
+  # Select only the interesting columns 
+  selected_columns = filtered_df[['Codice_Scarico', 'Lat', 'Long','Carico_Ingresso_AE', 'Nome_scarico','Regione']]
+
+  depths = []
+  Savg = []
+  Sstd = []
+  dilut_fac = 1.
+  for scol in range(selected_columns.shape[0]):
+      lat_df = selected_columns.Lat.to_numpy()[scol]
+      lon_df = selected_columns.Long.to_numpy()[scol]
+      #j = np.nanargmin(np.abs(lat_df - sal.latitude.values)) ##
+      #i = np.nanargmin(np.abs(lon_df - sal.longitude.values)) ##
+      idx = np.nanargmin((Lon-lon_df)**2 + (Lat-lat_df)**2)
+      #if np.shape(lon_df) == ():
+      #	j, i = idx//1, idx%1
+      #else:
+      #	j, i = idx//len(lon_df), idx%len(lon_df)
+      j, i = idx//len(sal.longitude.values), idx%len(sal.longitude.values)
+      k = bathyCMS[j, i].values
+      kn = int(np.where(np.isnan(k), 0, k - 1))
+      depths.append(sal.depth[kn].values * k / k)
+      Savg.append(np.mean(sal.so[:, kn, j, i].values * k / k))
+      Sstd.append(np.std(sal.so[:, kn, j, i].values * k / k))
+
+  selected_columns.insert(5, "CMS_depth", depths, True)
+  selected_columns.insert(6, "CMS_avgS", Savg, True)
+  selected_columns.insert(7, "CMS_stdS", Sstd, True)
+  selected_columns.insert(8, "Dilution_factor", dilut_fac, True)
+
+  # Convert the selected rows to a list of dictionaries
+  filtered_rows = selected_columns.to_dict(orient='records')
+
+  # Create the final JSON structure
+  output_data = {
+      "file_name_origin": file_name,
+      "domain_number": id+1,
+      "domain_name": domains[id],
+      "n_points": len(filtered_rows),
+      "discharge_points": filtered_rows
+  }
+  # build the output file name
+  output_file = base_path + domains[id] + '/PointSource_wSalt_' + domains[id] + '.json'
+  # Write the final JSON structure to a JSON file
+  with open(output_file, 'w') as json_file:
+      json.dump(output_data, json_file, indent=4)
+    
+  print(f"JSON file created: {output_file}")
+
