8effd22d9975c474bfafbf4815de206149330f65
Fabio Giordano <fgiordano@ogs.it>
2025-05-20 18:03:50 +0200

    Modified launcher.sh to incorporate changes to python scripts; completed both fiumi_json_gen.py, which needs additional info provided by the listaFIUMI_MER.xlsx (from gcossarini), and rbcs_gen.py, which generates binaries for: bottom salinity relaxation values, mask for this relaxation and N+M concentration files (N = # of point sources, M = # of rivers), as well as an aggregated netCDF for checking

--- src/mitgcm_inputs/rbcs/launcher.sh
+++ src/mitgcm_inputs/rbcs/launcher.sh
@@ -6,22 +6,25 @@
 JSON_DIR=$PWD/JSON_DIR
 mkdir -p $JSON_DIR
 
-#python ${script_path}read_excellistaFIUMI_do_json_for_7domains.py ${base_path} ${script_path}
-python3 scarichi_json_gen.py -i $PWD/Allegato_1_Capitolato_Tecnico_B32_B35_scarichi.xlsx -o $JSON_DIR
+for dom in ${domains}
+do
+        mkdir -p ${dom}
+        wget ${URL}/${dom}/MIT_static.nc ###
+        wget ${URL}/${dom}/rivers_positions.json ###
+        mv MIT_static.nc ${dom}/MIT_static_${dom}.nc ###
+        mv rivers_positions.json ${dom}/rivers_positions_${dom}.json ###
+done
 
+python scarichi_json_gen.py -i $PWD/Allegato_1_Capitolato_Tecnico_B32_B35_scarichi.xlsx -o $JSON_DIR ###
+python fiumi_json_gen.py -i $PWD/listaFIUMI_MER.xlsx -o $JSON_DIR ###
 
 URL=https://medeaf.ogs.it/internal-validation/gbolzon/MER/Domain_static_data
 domains="GOT GSN ION LIG NAD SAD SAR SIC TYR"
 
+
 for dom in ${domains}
 do
-        mkdir -p ${dom}
-        wget ${URL}/${dom}/MIT_static.nc
-        wget ${URL}/${dom}/rivers_positions.json
-        mv MIT_static.nc ${dom}/MIT_static_${dom}.nc
-        mv rivers_positions.json ${dom}/rivers_positions_${dom}.json
-
-        python3 -u rbcs_gen.py -i $JSON_DIR -d ${dom} --domdir $PWD/${dom}
+        python -u rbcs_gen.py -i $JSON_DIR -d ${dom} --domdir $PWD/${dom}
         echo ${dom}" done!"
 done
 
--- src/mitgcm_inputs/rbcs/rbcs_gen.py
+++ src/mitgcm_inputs/rbcs/rbcs_gen.py
@@ -100,7 +100,16 @@ def open_river_sources(
 
 	with open(json_file, 'r') as jfile:
 		jdata = json.load(jfile)
+	return jdata
+
+def open_old_rivers(
+		json_file,
+):
+	print(json_file)
+	with open(json_file, 'r') as jfile:
+		jdata = json.load(jfile)
 	return jdata['river_points']
+
 
 #
 
@@ -120,57 +129,21 @@ def fill_sal_conc(
 	fills the relaxation salinity array, creates the salinity mask and
 	fills the concentration array.
 	"""
-	if only_one:
-		jd = json_data[0]
+
+	n_sources = len(json_data)
+	conc_list = [conc] * n_sources
+
+	for ns, jd in enumerate(json_data):
 		lon = jd['Long']
 		lat = jd['Lat']
 		i = np.argmin(np.abs(x.values - lon))
 		j = np.argmin(np.abs(y.values - lat))
-		k = np.argmin(np.abs(z.values + depth[j, i].values))
+		k = np.argmin(np.abs(z.values + depth[j,i].values))
 		rel_S = jd['CMS_avgS'] - water_freshener
 		c = jd['Carico_Ingresso_AE'] * jd['Dilution_factor']
-		relax_sal[k, j, i] = rel_S
-		conc[j, i] = c
-	else:
-		for jd in json_data:
-			lon = jd['Long']
-			lat = jd['Lat']
-			i = np.argmin(np.abs(x.values - lon))
-			j = np.argmin(np.abs(y.values - lat))
-			k = np.argmin(np.abs(z.values + depth[j,i].values))
-			rel_S = jd['CMS_avgS'] - water_freshener
-			c = jd['Carico_Ingresso_AE'] * jd['Dilution_factor']
-			relax_sal[k,j,i] = rel_S
-			conc[j,i] = c
-	return relax_sal, xr.where(relax_sal == 0., 0., 1.), conc
-
-#
-
-def write_binary_files(
-		relax_sal,
-		S_mask,
-		conc,
-		aggregated = True,
-		out_dir = 'data/',
-		relax_path = 'relax_salinity.bin',
-		conc_path = 'tracer_concetrations.bin',
-		mask_path = 'S_source_mask_MER_V3.dat',
-		aggreg_path = 'sewage_discharges.nc',
-		conc_in_time = 365,
-):
-	"""
-	Given the the relaxation salinity, salinity mask and concentration
-	arrays, writes them to MITgcm-appropriate binary files.
-	"""
-	
-	relax_sal.values.astype('f4').tofile(out_dir / relax_path)
-	if conc_in_time == 1:
-		conc.values.astype('f4').tofile(out_dir / conc_path)
-	else:
-		np.stack([conc.values]*conc_in_time).astype('f4').tofile(out_dir / conc_path)
-	S_mask.values.astype('f4').tofile(out_dir / mask_path)
-	if aggregated:
-		xr.merge([relax_sal.rename('relax_salinity'), conc.rename('tracer_conc')]).to_netcdf(out_dir / aggreg_path)
+		relax_sal[k,j,i] = rel_S
+		conc_list[ns][j,i] = c
+	return relax_sal, xr.where(relax_sal == 0., 0., 1.).astype('f4'), conc_list
 
 #
 
@@ -181,36 +154,87 @@ def fill_river_conc(
 		z,
 		depth,
 		json_data,
-		water_freshener = 0.5,
-		only_one = False,
+		discharge_json,
+		uniform_concentration = 1000.,
 ):
 	"""
 	Given the info for the point sources and the spatial static data,
 	fills the relaxation salinity array, creates the salinity mask and
 	fills the concentration array.
 	"""
-	#TODO...
-	if only_one:
-		jd = json_data[0]
-		lon = jd['Long']
-		lat = jd['Lat']
-		i = np.argmin(np.abs(x.values - lon))
-		j = np.argmin(np.abs(y.values - lat))
-		k = np.argmin(np.abs(z.values + depth[j, i].values))
-		c = jd['Carico_Ingresso_AE'] * jd['Dilution_factor']
-		relax_sal[k, j, i] = rel_S
-		conc[j, i] = c
-	else:
-		for jd in json_data:
-			lon = jd['Long']
-			lat = jd['Lat']
-			i = np.argmin(np.abs(x.values - lon))
-			j = np.argmin(np.abs(y.values - lat))
-			k = np.argmin(np.abs(z.values + depth[j,i].values))
-			c = jd['Carico_Ingresso_AE'] * jd['Dilution_factor']
-			relax_sal[k,j,i] = rel_S
-			conc[j,i] = c
-	return relax_sal, xr.where(relax_sal == 0., 0., 1.), conc
+
+	n_sources = len(json_data)
+	conc_list = []
+	for ns in range(n_sources):
+		conc_list.append(conc * 0)
+
+	for ns, jd in enumerate(json_data): #[direc]
+		print('\n')
+		print(ns)
+		i = np.array(jd['longitude_indices']) #i = jd['longitude']
+		if isinstance(i, str):
+			rr = range(int(i[2:4]), int(i[-4:-1]))
+			i = np.array([ii for ii in rr])
+		j = np.array(jd['latitude_indices']) #j = jd['latitude']
+		if jd['side'] == 'E':
+			i -= 1
+		elif jd['side'] == 'W':
+			i += 1
+		elif jd['side'] == 'S':
+			j += 1
+		elif jd['side'] == 'N':
+			j -= 1
+		k = np.argmin(np.abs(z.values - depth[j, i].values))
+		print(j, i)
+		try:
+			cntr = 0
+			while discharge_json[cntr]['rivername'] != jd['name']:
+				cntr += 1
+			print(cntr)
+			print(discharge_json[cntr]['rivername'])
+			c = discharge_json[cntr]['MEAN_2011_2023'] * uniform_concentration
+			print(c)
+		except:
+			c = 50.e3
+		#relax_sal[k, j, i] = rel_S
+		conc_list[ns][j,i] += c
+		print(conc_list[ns].max().values)
+	print(np.unique(conc_list[1] == conc_list[0]))
+	return conc_list #relax_sal, xr.where(relax_sal == 0., 0., 1.), conc
+
+#
+
+def write_binary_files(
+		relax_sal,
+		S_mask,
+		concs,
+		out_dir,
+		aggregated=True,
+		relax_path='bottom_sources_S_relaxation.bin',
+		conc_path='tracer_concetrations.bin',
+		mask_path='bottom_sources_S_mask.bin',
+		aggreg_path='sewage_discharges.nc',
+		conc_in_time=365,
+):
+	"""
+	Given the relaxation salinity, salinity mask and concentration
+	arrays, writes them to MITgcm-appropriate binary files.
+	"""
+
+	relax_sal.values.astype('f4').tofile(out_dir / relax_path)
+	for i, conc in enumerate(concs):
+		if conc_in_time == 1:
+			conc.values.astype('f4').tofile(out_dir / (conc_path[:-4] + f'{i:02}' + conc_path[-4:]))
+		else:
+			np.stack([conc.values] * conc_in_time).astype('f4').tofile(
+				out_dir / (conc_path[:-4] + f'{i:02}' + conc_path[-4:]))
+
+	S_mask.values.astype('f4').tofile(out_dir / mask_path)
+
+	if aggregated:
+		xr.merge([S_mask.rename('salinity_mask'), relax_sal.rename('relax_salinity')] +
+				 [conc.rename(f'CONC{i:02}') for i, conc in enumerate(concs)]).to_netcdf(out_dir / aggreg_path)
+
 
 #
 
@@ -221,29 +245,40 @@ def fill_river_conc(
 base_path = args.inputdir
 domain = args.domain
 
+def main():
+	inputfile = args.domdir / ('MIT_static_' + domain + '.nc')
 
-inputfile=args.domdir / ('MIT_static_' + domain + '.nc')
-relax_salt = initialise_sal(xr.open_dataset(inputfile).hFacC)
-tracer_conc = initialise_conc_fluxes(xr.open_dataset(inputfile).hFacC)
-coords = get_spatial_masks(xr.open_dataset(inputfile))
+	# sewage
+	sewage_path = args.inputdir / f'PointSource_{domain}.json'
+	sewers = open_point_sources(sewage_path)
 
-sewage_path = args.inputdir / f'PointSource_{domain}.json'
-sewers = open_point_sources(sewage_path)
+	relax_salt = initialise_sal(xr.open_dataset(inputfile).hFacC)
+	tracer_conc = initialise_conc_fluxes(xr.open_dataset(inputfile).hFacC)
+	coords = get_spatial_masks(xr.open_dataset(inputfile))
 
-relax_salt, mask_salt, tracer_conc = fill_sal_conc(relax_salt, tracer_conc, coords['xc'], coords['yc'], coords['zc'], coords['depth'], sewers, only_one=True)
-write_binary_files(relax_salt, mask_salt, tracer_conc, out_dir=args.domdir,
-				   relax_path='bottom_sources_S_relaxation_'+domain+'.bin', conc_path='conc01_bottom_fluxes_'+domain+'.bin',
-				   mask_path='bottom_sources_S_mask_'+domain+'.bin', aggreg_path='sewage_discharges_'+domain+'.bin')
+	relax_salt, mask_salt, concentrations = fill_sal_conc(relax_salt, tracer_conc, coords['xc'], coords['yc'], coords['zc'], coords['depth'], sewers)#, only_one=True)
+	#write_binary_files(relax_salt, mask_salt, tracer_conc, out_dir=args.domdir, conc_path='conc01_bottom_fluxes.bin',)
+
+	# rivers
+	rivers_path = args.domdir / f'rivers_positions_{domain}.json'
+	old_rivers_path = args.inputdir / f'RiverSource_{domain}.json'
+	rivers = open_river_sources(rivers_path)
+	old_rivers = open_old_rivers(old_rivers_path)
+
+	tracer_conc = initialise_conc_fluxes(xr.open_dataset(inputfile).hFacC)
+	concentrations = concentrations + fill_river_conc(tracer_conc, coords['xc'], coords['yc'], coords['zc'], coords['depth'], rivers, old_rivers) #, only_one=True
+	print(type(concentrations), len(concentrations))
+
+	write_binary_files(relax_salt, mask_salt, concentrations, out_dir=args.domdir, conc_path='CONC.bin',
+					   aggreg_path='check_fluxes.nc')
+
+
+
+
 #
-#rivers_path = base_path + domain + f'/RiverSource_{domain}.json'
-#rivers = open_river_sources(rivers_path)
-
-
-
 #
-#
-# if __name__ == '__main__':
-# 	main()
-# 	print('DONE!')
+if __name__ == '__main__':
+	main()
+	print('DONE!')
 
 
