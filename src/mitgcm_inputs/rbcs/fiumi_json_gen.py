85af65ad678a576e1e84e254674d86c1d76864a5
Giorgio Bolzon <gbolzon@ogs.it>
2025-05-30 12:20:01 +0200

    improving indentation an docstring in fiumi_json_gen.py

--- src/mitgcm_inputs/rbcs/fiumi_json_gen.py
+++ src/mitgcm_inputs/rbcs/fiumi_json_gen.py
@@ -7,10 +7,9 @@ from bitsea.utilities.argparse_types import existing_dir_path
 def argument():
     parser = argparse.ArgumentParser(description = """
     Reads seawage info on excel file and for all the domains
-    Takes salinity data from MDS service
     build the json files with the coordinate of the pointsource for E.Coli tracers
-    PointSource_NAD.json
-    PointSource_SAD.json
+    RiverSource_NAD.json
+    RiverSource_SAD.json
     ...
 
     """)
@@ -43,41 +42,40 @@ OUTDIR = args.outdir
 # select the n. of domain from 1 to 7 and name the json file
 
 domains=['NAD','SAD','ION','SIC','TYR','LIG','SAR', 'GoT', 'GSN']
-#domains = ['LIG']
 
 # Read the Excel file into a DataFrame
 df = pd.read_excel(excel_file)
 
 for id, namedomain in enumerate(domains):
 
-  '''
-  # Filter rows where the value in column 'Dominio' is id+1 (valid number from 1 to 7)
-  if id < 7:
-      filtered_df = df[df['Dominio'] == id+1]
-  else:
-      filtered_df = df[df['Sotto-Dominio'] == 8]  ### hardcoded horribly !!!
-  '''
-  filtered_df = df*1
-
-  # Select only the interesting columns 
-  selected_columns = filtered_df[['rivername', 'lat_mouth', 'lon_mouth','MEAN_2011_2023', 'catchment','Region']]
-
-  # Convert the selected rows to a list of dictionaries
-  filtered_rows = selected_columns.to_dict(orient='records')
-
-  # Create the final JSON structure
-  output_data = {
-      "file_name_origin": excel_file.name,
-      "domain_number": id+1,
-      "domain_name": domains[id],
-      "n_points": len(filtered_rows),
-      "river_points": filtered_rows
-  }
-  output_file = OUTDIR / ('RiverSource_' + domains[id] + '.json')
-  # build the output file name
-  # Write the final JSON structure to a JSON file
-  with open(output_file, 'w') as json_file:
-      json.dump(output_data, json_file, indent=4)
+    '''
+    # Filter rows where the value in column 'Dominio' is id+1 (valid number from 1 to 7)
+    if id < 7:
+        filtered_df = df[df['Dominio'] == id+1]
+    else:
+        filtered_df = df[df['Sotto-Dominio'] == 8]  ### hardcoded horribly !!!
+    '''
+    filtered_df = df*1
     
-  print(f"JSON file created: {output_file}")
+    # Select only the interesting columns
+    selected_columns = filtered_df[['rivername', 'lat_mouth', 'lon_mouth','MEAN_2011_2023', 'catchment','Region']]
+
+    # Convert the selected rows to a list of dictionaries
+    filtered_rows = selected_columns.to_dict(orient='records')
+
+    # Create the final JSON structure
+    output_data = {
+        "file_name_origin": excel_file.name,
+        "domain_number": id+1,
+        "domain_name": domains[id],
+        "n_points": len(filtered_rows),
+        "river_points": filtered_rows
+    }
+    output_file = OUTDIR / ('RiverSource_' + domains[id] + '.json')
+    # build the output file name
+    # Write the final JSON structure to a JSON file
+    with open(output_file, 'w') as json_file:
+        json.dump(output_data, json_file, indent=4)
+
+    print(f"JSON file created: {output_file}")
 
