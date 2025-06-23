936cf8bd5c2742ff74bea2eed0e14e81deb69df9
Giorgio Bolzon <gbolzon@ogs.it>
2025-06-23 15:59:18 +0200

    Fixing generation tar.gz files

--- src/mitgcm_inputs/rbcs/launcher.sh
+++ src/mitgcm_inputs/rbcs/launcher.sh
@@ -30,13 +30,16 @@ done
 
 
 # generation of files for https://medeaf.ogs.it/internal-validation/gbolzon/MER/Domain_static_data
+HERE=$PWD
 for dom in ${domains} ; do
         cd ${dom}
+        echo ${dom}
         mkdir -p conc relax
-        mv conc*bin conc/
-        mv bottom*bin relax/
-        tar -cf conc.tar conc/
-        tar -cf relax.tar relax/
-        gzip -c conc.tar > conc.tar.gz && rm -f conc.tar
-        gzip -c relax.tar > relax.tar.gz && rm -f relax.tar
+        my_prex "mv conc*bin conc/"
+        my_prex "mv bottom*bin relax/"
+        my_prex_or_die "tar -cf conc.tar conc/"
+        my_prex_or_die "tar -cf relax.tar relax/"
+        my_prex_or_die "gzip -c conc.tar > conc.tar.gz && rm -f conc.tar"
+        my_prex_or_die "gzip -c relax.tar > relax.tar.gz && rm -f relax.tar"
+        cd $HERE
 done
