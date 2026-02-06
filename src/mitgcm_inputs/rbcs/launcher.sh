d7a3bfa27d424b98ab759ad59e7209053d0d68d4
Giorgio Bolzon <gbolzon@ogs.it>
2026-02-06 21:34:19 +0100

    The output RBCS_tracer_names.json is replaced by RBCS_names.txt This file is aimed at being copied in domain.json mer files.
    Ecoli info comes now from bathytools/main_rivers.json, of all Italian rivers.
    Refactoring get_river_swg_plume()

--- src/mitgcm_inputs/rbcs/launcher.sh
+++ src/mitgcm_inputs/rbcs/launcher.sh
@@ -5,26 +5,26 @@
 
 . ./profile.inc
 
-domains="ION LIG NAD SAD SAR SIC TYR GOT GSN"
-domains="GSN"
+domains="ION LIG NAD SAD SAR SIC TYR GOT GSN ISO CAG"
+domains="ION"
 URL=https://medeaf.ogs.it/internal-validation/gbolzon/MER/Domain_static_data
 URL=/leonardo_work/OGS_test2528_0/MER/bathytools/
+URL=/leonardo_work/OGS_test2528_0/pub/gbolzon/MER/Domain_static_data/
 
+curl "https://raw.githubusercontent.com/inogs/bathytools/refs/heads/main/mer_domains/rivers/main.json" > rivers_main.json
 for dom in ${domains} ; do
         domdir=${PWD}/${dom}
         mkdir -p $domdir
-	cp $URL/${dom}/MIT_static.nc ${domdir}
-	cp ${URL}/${dom}/rivers_positions.json ${domdir}
-	cp ${URL}/${dom}/meshmask.nc ${domdir}
+	my_prex_or_die "cp $URL/${dom}/MIT_static.nc ${domdir}/MIT_static.nc"
+	my_prex_or_die "cp ${URL}/${dom}/rivers_positions.json ${domdir}"
+	my_prex_or_die "gzip -dc ${URL}/${dom}/meshmask.nc.gz > ${domdir}/meshmask.nc"
         #wget ${URL}/${dom}/MIT_static.nc -O ${domdir}/MIT_static.nc ###
         #wget ${URL}/${dom}/meshmask.nc -O ${domdir}/meshmask.nc ###
         #wget ${URL}/${dom}/rivers_positions.json -O ${domdir}/rivers_positions.json ###
-        curl "https://raw.githubusercontent.com/inogs/bathytools/refs/heads/main/mer_domains/rivers/${dom}.json" > ${domdir}/rivers.json
 
 
-        rivers_json=${domdir}/rivers.json
         maskfile=${domdir}/meshmask.nc
         sewers_json=${domdir}/PointSource.json
         my_prex_or_die "python scarichi_json_gen.py -i $PWD/Allegato_1_Capitolato_Tecnico_B32_B35_scarichi.xlsx -d ${domdir} -m $maskfile -o $sewers_json"
-        my_prex_or_die "python -u rbcs_gen.py -s $sewers_json -r $rivers_json --domdir ${domdir}"
+        my_prex_or_die "python -u rbcs_gen.py -s $sewers_json -r rivers_main.json --domdir ${domdir}"
 done
