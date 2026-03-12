442a48600435e5c1738155595947237be9725008
Stefano Piani <spiani@ogs.it>
2026-03-12 17:01:29 +0100

    Parallelized generate_mer_statics.sh

--- launcers/all_statics/generate_mer_statics.sh
+++ launcers/all_statics/generate_mer_statics.sh
@@ -12,57 +12,80 @@ DOMAINS_HR="HGT GOR CON PES LAM PAN NAP GAE FOL CAG ISO"
 
 DOMAINS="$DOMAINS_REG $DOMAINS_HR"
 
-mkdir -p ${OUTPUTDIR}
+MAXJOBS=5   # numero massimo di domini processati in parallelo
 
-for domain in ${DOMAINS}
-  do
+mkdir -p "${OUTPUTDIR}"
+
+process_domain () {
+    set -e
+    domain=$1
+
     domaindir=${OUTPUTDIR}/${domain}
-    mkdir ${domaindir}
+    rm -rf "${domaindir}"
+    mkdir -p "${domaindir}"
 
     echo
     echo "----------^^^-----------  DOMAIN ${domain}  ----------^^^-----------"
     echo
 
-    # Setta il valore dello SPONGE
+    # Setta SPONGE
     if [[ " $DOMAINS_HR " =~ " $domain " ]]; then
         SPONGE=10
     else
         SPONGE=16
     fi
 
-    echo SPONGE $SPONGE
+    cd "$BATHYTOOLSDIR"
 
-    cd $BATHYTOOLSDIR
-    # Check where is the correct configuration file for the domain (in the mer_domains
-    # directory or in the mer_domains/high_reso dir)
     domain_descriptor="$BATHYTOOLSDIR/mer_domains/${domain,,}.yaml"
     if [ ! -f "${domain_descriptor}" ]; then
         domain_descriptor="$BATHYTOOLSDIR/mer_domains/high_reso/${domain,,}.yaml"
     fi
 
-    # Run bathytools
-    poetry run bathytools -c ${domain_descriptor} -o ${domaindir} --mer
+    poetry run bathytools -c "${domain_descriptor}" -o "${domaindir}" --mer
 
-    # Generate fluxes.tar.gz
-    cd ${MITGCMINPUTSDIR}
-    poetry run mitgcm_inputs FLUXES -m ${domaindir}/meshmask.nc -o ${domaindir}/fluxes.tar.gz
+    cd "$MITGCMINPUTSDIR"
 
-    # Check if this domain has rivers to write the rbcs and ob_indices command
-    # line options
-    rpositions=${domaindir}/rivers_positions.json
-    rcustom=${BATHYTOOLSDIR}/mer_domains/rivers/${domain}.json
+    poetry run mitgcm_inputs FLUXES \
+        -m "${domaindir}/meshmask.nc" \
+        -o "${domaindir}/fluxes.tar.gz"
+
+    rpositions="${domaindir}/rivers_positions.json"
+    rcustom="${BATHYTOOLSDIR}/mer_domains/rivers/${domain}.json"
+
     rbcs_options=""
     ob_indices_options=""
 
     if [ -f "$rpositions" ]; then
         rbcs_options="-p $rpositions -r $BATHYTOOLSDIR/mer_domains/rivers/main.json"
-        ob_indices_options="-p $rpositions"
+        ob_indices_options="-p $rpositions -r ${domaindir}/additional_variables.nc"
+
         if [ -f "${rcustom}" ]; then
             rbcs_options="${rbcs_options} -d ${rcustom}"
         fi
     fi
 
-    poetry run mitgcm_inputs ob_indices -m ${domaindir}/meshmask.nc -r ${domaindir}/additional_variables.nc -s $SPONGE -o ${domaindir}/rivers_open_boundaries.txt -n ${domaindir}/nudging_indices.txt ${ob_indices_options}
+    poetry run mitgcm_inputs ob_indices \
+        -m "${domaindir}/meshmask.nc" \
+        -s "$SPONGE" \
+        -o "${domaindir}/rivers_open_boundaries.txt" \
+        -n "${domaindir}/nudging_indices.txt" \
+        ${ob_indices_options}
 
-    poetry run mitgcm_inputs rbcs -m ${domaindir}/meshmask.nc -o ${domaindir} ${rbcs_options}
-  done
+    poetry run mitgcm_inputs rbcs \
+        -m "${domaindir}/meshmask.nc" \
+        -o "${domaindir}" \
+        ${rbcs_options}
+
+}
+
+export -f process_domain
+export OUTPUTDIR BATHYTOOLSDIR MITGCMINPUTSDIR DOMAINS_HR
+
+parallel --lb --tag --halt soon,fail=1 -j ${MAXJOBS} process_domain ::: ${DOMAINS}
+
+### Serial version if parallel is missing
+# for domain in $DOMAINS
+#   do
+#     process_domain $domain
+#   done
